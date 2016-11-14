d36e335dbe9e134e2707c4ed9eb14a16f868e94f
Joost van Griethuysen <j.v.griethuysen@nki.nl>
2016-11-14 17:29:37 -0500

    BUG: Numpy needed in setup
    Fix multiple bugs preventing compiling in Linux. Additionally, add extension to root folder of pyradiomics (otherwise, nosetests cannot find _cmatrices, as radiomics package is loaded from source folder, not the installed path).

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 import versioneer
 from distutils import sysconfig
 from setuptools import setup, Extension
+import numpy
 
 with open('requirements.txt', 'r') as fp:
   requirements = list(filter(bool, (line.strip() for line in fp)))
 
-incDirs = [sysconfig.get_python_inc(), sysconfig.get_python_lib()]
+incDirs = [sysconfig.get_python_inc(), numpy.get_include()]
 setup(name='pyradiomics',
       version=versioneer.get_version(),
       cmdclass=versioneer.get_cmdclass(),
@@ -13,13 +14,13 @@ setup(name='pyradiomics',
       data_files=[
         ('data', ['data/paramSchema.yaml', 'data/schemaFuncs.py', 'data/brain1_image.nrrd', 'data/brain1_label.nrrd'])],
       description='Radiomics features library for python',
-      ext_modules=[Extension("radiomics._cmatrices",
+      ext_modules=[Extension("_cmatrices",
                              ["radiomics/src/_cmatrices.c", "radiomics/src/cmatrices.c"],
-                             include_dirs=incDirs)],
+                             include_dirs=incDirs, extra_compile_args=['-std=c99'])],
       url='http://github.com/Radiomics/pyradiomics',
       author='pyradiomics community',
       author_email='pyradiomics@googlegroups.com',
       license='Slicer',
       zip_safe=False,
-      setup_requires=['cython'],
+      setup_requires=['cython', 'numpy>=1.10.0'],
       install_requires=requirements)
